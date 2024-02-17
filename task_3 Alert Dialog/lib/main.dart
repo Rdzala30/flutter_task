cb35708c8ed36af1b519861452b210d698c219df
Rajpalsinh zala <rdzala30@gmail.com>
2024-02-17 18:19:42 +0530

    17 feb

---
+++ task_3 Alert Dialog/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'search.dart';
+import 'SideNavBar.dart'; // Import the SideNavBar file
+import 'Alerts.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        children: [
+          // Your side tab bar goes here
+          SideNavBar(
+            currentIndex: _currentIndex,
+            onTabTapped: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+
+          // Your main content goes here
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Center(
+                child: _buildMainContent(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMainContent() {
+    switch (_currentIndex) {
+      case 1:
+        return Text('Cloud Content');
+      case 2:
+        return AlertPage();
+      case 3:
+        return SearchPage();
+      case 4:
+        return Text('Research Book Content');
+      case 5:
+        return Text('Profile Content');
+      case 6:
+        return Text('Logout Content');
+      default:
+        return Text('Main Content');
+    }
+  }
+}
---
+++ task_3 Alert Dialog/lib/search.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'Facts.dart';
+import 'Objectives.dart';
+// import 'main_contenrt.dart';
+
+
+class SearchPage extends StatelessWidget {
+  // Define your list of options
+  List<String> options = [
+    'Facts  +Add new',
+    'Objective ',
+    'Stages',
+    'Filling of plaint ',
+    ' Framing of issues',
+    'Execution of Decreed',
+    'Acts and Section',
+    'Relevancy by BharatLaw ',
+    'Legal Strategy',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Antony Vs K.G. Raghavan Nair",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(10, 0, 77, 10.0),
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+      body: Scrollbar(
+        child: Row(
+          children: [
+            // First Child (Vertical List - Option Menu)
+            Container(
+              width: screenWidth * 0.2,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: options.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 45, // Set the height as needed
+                    margin: EdgeInsets.all(2.0),
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Text(
+                        options[index],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: MainTextPage(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
